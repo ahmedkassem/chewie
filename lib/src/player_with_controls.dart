7e08fea2a58870baa00c371a7d339c65ae618d75
Ahmed Kassem <kassem@gmail.com>
2021-05-31 02:47:12 +0200

    Only iOS Controls

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -26,12 +26,15 @@ class PlayerWithControls extends StatelessWidget {
       BuildContext context,
       ChewieController chewieController,
     ) {
-      final controls = Theme.of(context).platform == TargetPlatform.android
-          ? const MaterialControls()
-          : const CupertinoControls(
-              backgroundColor: Color.fromRGBO(41, 41, 41, 0.7),
-              iconColor: Color.fromARGB(255, 200, 200, 200),
-            );
+      //!! Only iOS Controls
+      final controls =
+          // Theme.of(context).platform == TargetPlatform.android
+          //     ? const MaterialControls()
+          //     :
+          const CupertinoControls(
+        backgroundColor: Color.fromRGBO(41, 41, 41, 0.7),
+        iconColor: Color.fromARGB(255, 200, 200, 200),
+      );
       return chewieController.showControls
           ? chewieController.customControls ?? controls
           : Container();
